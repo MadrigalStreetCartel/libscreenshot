55cb4db164f20f4f7f203dd231742824f82531c1
SplittyDev <splittydev@gmail.com>
2022-07-06 17:46:52 +0200

    Add macOS bindings

---
+++ macos-bindings/build.rs
@@ -0,0 +1,39 @@
+use std::{env, io::Error, path::Path, process::Command};
+
+fn get_sdk_path() -> Result<String, Error> {
+    let output = Command::new("xcrun")
+        .args(["--sdk", "macosx", "--show-sdk-path"])
+        .output()?
+        .stdout;
+    Ok(String::from_utf8(output).unwrap().trim().to_string())
+}
+
+pub fn main() {
+    let target = std::env::var("TARGET").unwrap();
+    let sdk_path = get_sdk_path().unwrap();
+
+    println!("cargo:rustc-link-lib=framework=AppKit");
+
+    let builder = bindgen::Builder::default()
+        .rustfmt_bindings(true)
+        .header_contents(
+            "NSWorkspace.h",
+            "
+            #include<AppKit/NSWorkspace.h>
+            #include<AppKit/NSRunningApplication.h>
+        ",
+        )
+        .clang_arg(format!("--target={}", target))
+        .clang_args(&["-isysroot", sdk_path.as_ref()])
+        .block_extern_crate(true)
+        .objc_extern_crate(true)
+        .clang_arg("-ObjC")
+        .blocklist_item("objc_object");
+
+    let bindings = builder.generate().unwrap();
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    bindings
+        .write_to_file(Path::new(&out_dir).join("nsworkspace.rs"))
+        .unwrap();
+}
