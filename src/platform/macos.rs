1ea812315256318db2e516671a0410e6b33e7e83
SplittyDev <splittydev@gmail.com>
2022-07-06 23:20:51 +0200

    Add macOS helpers

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -3,3 +3,20 @@ mod cg_provider;
 
 #[cfg(feature = "macos")]
 pub use self::cg_provider::CGProvider;
+
+#[cfg(feature = "macos")]
+pub mod macos_helper {
+    use std::ffi::c_void;
+
+    use cocoa::{appkit::NSEvent, base::id};
+    use core_graphics::window::CGWindowID;
+
+    pub unsafe fn ns_window_to_window_id(ptr: *const c_void) -> Option<CGWindowID> {
+        let id = ptr as id;
+        if !id.is_null() {
+            Some(id.windowNumber() as u32)
+        } else {
+            None
+        }
+    }
+}
